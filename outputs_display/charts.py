74bbcfb513549469a537d63130d5188d5abdcdbe
AlexisBaudin <alexis.baudin@live.fr>
2019-09-02 20:59:49 +0200

    Scenario data + charts.py change name

--- outputs_display/charts.py
+++ outputs_display/charts.py
@@ -19,65 +19,62 @@ def red_name(line_name):
         cpt_char += 1
     return name_red
     
-def create_bar_charts(time_steps, values, gen_title, legend, colors, nb_folds):
+def create_bar_charts(time_steps, values, gen_title, legend, nb_folds):
+
     # carré supérieur
     lines_to_draw = list(values.keys())
-    nb_lines = int(sqrt(len(lines_to_draw))) + 1 #min(3,len(lines_to_draw))
+    nb_lines = int(sqrt(len(lines_to_draw))) + 1 
     # nombre de col qui complète le nombre de lignes
-    nb_col = ((len(lines_to_draw)-1) // nb_lines +1) # * len(time_steps)
+    nb_col = ((len(lines_to_draw)-1) // nb_lines +1)
+
     figsize=(nb_col*3,nb_lines*3)
     fig, axes = plt.subplots(nb_lines, nb_col, squeeze=False, figsize=figsize)
+
     for i, ax_row in enumerate(axes):
         for j, ax in enumerate(ax_row):
+
             if i*nb_col + j < len(lines_to_draw):
                 list_rect = [[] for _ in range(nb_folds)]
                 line = lines_to_draw[i*nb_col + j]
-                #deb=1
+                
                 for time in time_steps:
-                    # pour i qui parcourt val:
-                    # rect(i) enregistre val(i)
-                    # puis APRES on dessiner bien, avec les couleurs par défaut c'est ok
+                    
                     val = values[line][time]
                     for k in range(nb_folds):
                         list_rect[k].append(val[k])
-                    #index = range(deb,len(val)+deb)
-                    #deb += len(val)
-                    #ax.bar(index, val, color=col_time[time])
+                        
                 width = 0.8
                 index = np.arange(1,len(time_steps)+1)
-                #ax.bar(index - width/2, list_rect[0], width)
-                #ax.bar(index + width/2, list_rect[1], width)
 
                 pos = [index + k*width/nb_folds for k in range(nb_folds)]
 
                 for k in range(nb_folds):
-                 #   ax.bar(index, rect)    
-                     ax.bar(index + k*width/nb_folds, list_rect[k], width/nb_folds)#, color=colors[i])    
+                     ax.bar(index + k*width/nb_folds, list_rect[k], width/nb_folds)   
+
                 line_name_red = red_name(line)
                 ax.set_title(line_name_red)
-                #ax.set_xticks(range(1,len(label[line])+1))
-                #ax.set_xticklabels(label[line], rotation=45)
-                #ax.set_xticks(range(1,len(time_steps)+1))
+
                 lab = []
                 if (nb_folds % 2 == 1):
                     lab = pos[nb_folds // 2]
                 else:
                     lab = (pos[nb_folds // 2 - 1] + pos[nb_folds // 2]) / 2
                 ax.set_xticks(lab)
-                ax.set_xticklabels(time_steps)
+
+                labels = [x[-4:] for x in time_steps]
+                ax.set_xticklabels(labels, rotation=45)
+                
             else:
                 ax.axis('off')
 
-    #fig.legend(time_steps)
     fig.suptitle(gen_title)
-    fig.legend(legend, loc='lower center')#,labelspacing=0.1, fontsize='small')
+    fig.legend(legend, loc='lower center')
 
     plt.subplots_adjust(top=0.8, hspace=0.9, wspace=0.7, bottom=0.2)
 
     chart_path = save_path + gen_title + '.png'
     plt.savefig(chart_path)
     plt.close(fig)
-    #plt.show()
 
 
 def create_radar_charts(values, time_steps, title, colors, legend, nb_folds):
@@ -94,13 +91,9 @@ def create_radar_charts(values, time_steps, title, colors, legend, nb_folds):
 
     fig, axes = plt.subplots(figsize=(9, 9), nrows=2, ncols=1,
                             subplot_kw=dict(projection='radar'))
-                            #facecolor = 'white')
 
     fig.subplots_adjust(wspace=0.25, hspace=0.4, top=0.85, bottom=0.05)
 
-    #colors = ['b', 'r', 'g']#, 'm', 'y']
-    # Plot the four cases from the example data on separate axes
-    #for ax, (title, case_data) in zip(axes.flatten(), data):
     for k in range(len(axes.flatten())):
         ax = axes.flatten()[k]
         ax.set_rgrids([0.2, 0.4, 0.6, 0.8])
@@ -117,12 +110,10 @@ def create_radar_charts(values, time_steps, title, colors, legend, nb_folds):
                      val_data[time][i].append(values[line][time][i])
 
 
-            ax.plot(theta, val_data[time][i])#, color=colors[i])
-            #ax.fill(theta, d, facecolor=color, alpha=0.25)
+            ax.plot(theta, val_data[time][i])
+            
             ax.set_varlabels(spoke_labels)
-            #ax.set_facecolor('white')
-            # Go through labels and adjust alignment based on where
-            # it is in the circle.
+
             for lbl, angle in zip(ax.get_xticklabels(), theta):
                 if angle in (0, pi):
                     lbl.set_horizontalalignment('center')
@@ -131,24 +122,11 @@ def create_radar_charts(values, time_steps, title, colors, legend, nb_folds):
                 else:
                     lbl.set_horizontalalignment('left')
 
-    # add legend relative to top-left plot
-    #ax = axes[0, 0]
-    #list_lab = list(macro_prop.keys())
-    #labels = (list_lab[0], list_lab[1], list_lab[2])#, 'Factor 4', 'Factor 5')
-    #ax.legend(label[line], loc=(0.9, .95),
-    #                labelspacing=0.1, fontsize='small')
-
-    
-
-    #fig.text(0.5, 0.965, title,
-    #        horizontalalignment='center', color='black', weight='bold',
-    #        size='large')
-
     fig.suptitle(title)
     fig.legend(legend, loc='right')
 
     plt.savefig(save_path + 'radar_' + title + '.png')
-    #plt.show()
+
     plt.close(fig)
 
 def build_charts(file_name, charts_to_draw, save_path, colors=['b', 'r', 'g', 'm', 'y'], type_chart='bar', norm_ref=None):
@@ -161,7 +139,6 @@ def build_charts(file_name, charts_to_draw, save_path, colors=['b', 'r', 'g', 'm
     for chart in charts_to_draw.keys():
         lines_to_draw += charts_to_draw[chart]  
 
-    # TODO : fonction car besoin dans radar_chart
     # Keep the selected lines
     for fold in outputs.keys():
         for time in outputs[fold].keys():
@@ -178,7 +155,6 @@ def build_charts(file_name, charts_to_draw, save_path, colors=['b', 'r', 'g', 'm
             
             outputs[fold][time] = file
 
-    # TODO : fonction car besoin dans radar_chart
     # Convert the lines into numbers
     for fold in outputs.keys():
         for time in outputs[fold].keys():
@@ -190,27 +166,24 @@ def build_charts(file_name, charts_to_draw, save_path, colors=['b', 'r', 'g', 'm
                 raise Exception('Some elements are not numbers \
                     in the lines to draw selected.')
 
-    # TODO : autoriser tous les time step, et faire une liste avec TOUS, pour la légende
+    # Read time steps and return an error if inconsistency
     time_steps = ro.record_time_steps(outputs)
 
-    col_time = dict()
-    col_ind = 0
-    for time in time_steps:
-        col_time[time] = colors[col_ind]
-        col_ind += 1
 
-    # Create bar charts
+    # Create charts
     
     for chart in charts_to_draw.keys():
         lines_to_draw = charts_to_draw[chart]
 
-        # DATA
+        # Load data
         values_chart = dict()
-        label = dict()
+
         for line_name in lines_to_draw:
+
             values_chart[line_name] = dict()
-            label[line_name] = []
+
             for time in time_steps:
+
                 values_chart[line_name][time] = []
                 for fold in outputs.keys():
                     line = ro.get_line(outputs[fold][time], line_name)
@@ -222,20 +195,20 @@ def build_charts(file_name, charts_to_draw, save_path, colors=['b', 'r', 'g', 'm
                         val = val / line_ref[1]
 
                     values_chart[line_name][time].append(val)
-                    label[line_name].append(fold)
 
-
-
-        #print(values_chart)
 
         nb_folds = len(list(outputs.keys()))
 
         # BAR CHARTS
+
         if type_chart == 'bar':
-            create_bar_charts(time_steps, values_chart, chart, list(outputs.keys()), colors, nb_folds)
+            create_bar_charts(time_steps, values_chart, chart, list(outputs.keys()), nb_folds)
+
         # RADAR CHARTS
+
         elif type_chart == 'radar':
             create_radar_charts(values_chart, time_steps, chart, colors, list(outputs.keys()), nb_folds)
+
         else:
             raise Exception('type_chart unknown')
 
@@ -257,7 +230,6 @@ def data_macro():
             'Imports in GDP',
             'Imports/Domestic production ratio',
             'Imports of Non Energy goods in volume',
-            #'Exports of Non Energy goods in volume',
             'Real GDP (Laspeyres)', 
             'Households consumption in GDP',
             'Labour Intensity (Laspeyres)',
